cbb46e29d641fc2edf45f1ece6617e9f73aee511
SangHyeok <tkdgur7234@naver.com>
2024-04-19 23:01:31 +0900

    add print fucking line

--- test/src/test/helloworld.java
+++ test/src/test/helloworld.java
@@ -3,8 +3,8 @@ package test;
 public class helloworld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hello world");
+		System.out.prinln("Hello his fucking world");
 	}
 
 }
