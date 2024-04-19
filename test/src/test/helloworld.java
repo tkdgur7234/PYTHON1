34635789b602da5582ebce7ace5ba7195c7e90d1
SangHyeok <tkdgur7234@naver.com>
2024-04-19 23:04:07 +0900

    added "your fucking world"

--- test/src/test/helloworld.java
+++ test/src/test/helloworld.java
@@ -5,6 +5,7 @@ public class helloworld {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("Hello his fucking world");
+		System.out.println("Helloy your fucking world");
 	}
 
 }
