e2bfc654bb335b9b9b4ee5bf5723f2a3c40af84b
SangHyeok <tkdgur7234@naver.com>
2024-04-19 23:02:15 +0900

    add fucking print line

--- test/src/test/helloworld.java
+++ test/src/test/helloworld.java
@@ -4,7 +4,7 @@ public class helloworld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
-		System.out.prinln("Hello his fucking world");
+		System.out.println("Hello his fucking world");
 	}
 
 }
